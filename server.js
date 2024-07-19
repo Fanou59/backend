fa5cb4f5f7cdf145c753d255266fdfaf1dfcbacb
Ste[PH]ane <steph.guery@gmail.com>
2024-07-19 15:51:20 +0200

    2 routes ok

--- server.js
+++ server.js
@@ -9,14 +9,30 @@ fastify.register(fastifyPostgres, {
   connectionString: 'postgres://user:password@localhost:5432/mydatabase'
 })
 
-fastify.get('/titre/:id', function (req, reply) {
+fastify.get('/', function (req, reply){
+    reply.send({ hello : 'world' })
+})
+
+// choix selon un titre -> il faut encore gérer l'erreur si le titre n'est pas trouvé
+fastify.get('/list/:titre', function (req, reply) {
   fastify.pg.query(
-    'SELECT titre FROM mydatabase.aebersold WHERE id=$1', [req.params.id],
+    'SELECT titre,volume,vehicle, akey, tempo, chorus, disc, track FROM mydatabase.aebersold WHERE titre=$1', [req.params.titre],
     function onResult (err, result) {
-      reply.send(err || result)
+        const {titre, volume, vehicle, akey, tempo, chorus, disc, track} = result.rows[0]
+      reply.send(err || ({titre, volume, vehicle, akey, tempo, chorus, disc, track}))
     }
   )
 })
+
+// récupération de l'ensemble de la base de donnée
+fastify.get('/list', function (req, reply) {
+    fastify.pg.query(
+      'SELECT titre,volume,vehicle, akey, tempo, chorus, disc, track FROM mydatabase.aebersold',
+      function onResult (err, result) {
+        reply.send(err || result.rows)
+      }
+    )
+  })
 
 fastify.listen({ port: 3000 }, err => {
   if (err) throw err
